9b33ce82d8d71ab1c12bd2e41f822d200ae34713
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-02-15 23:38:13 -0500

    commit

--- Unit 1/excercises.java
+++ Unit 1/excercises.java
@@ -9,6 +9,7 @@ public class excercises {
 		 * A*x*x + B*x + C = 0, provided it has any roots.  If A == 0 or
 		 * if the discriminant, B*B - 4*A*C, is negative, then an exception
 		 * of type IllegalArgumentException is thrown.
+		 *
 		 */
 
 		/**
