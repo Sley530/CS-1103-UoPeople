b56e6f38a2dae6f3587c318a528e63cdef94dd53
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-02-21 23:05:38 -0500

    Update

--- Unit 4/BuggySearchAndSort.java
+++ Unit 4/BuggySearchAndSort.java
@@ -1,19 +1,19 @@
-package Unit_4;
+//package Unit_4;
 
 import java.util.Arrays;
 
 
 
 /**
- * 
- * 				
+ *
+ *
  * 				   			 Programming Assignment Unit 4
  * 								University of the People
  *								 Programming 2 CS 1103
  *								 Eman Omar, instructor
  *								   February 22, 2023
- * 										
- * 
+ *
+ *
  */
 
 
@@ -22,34 +22,34 @@ import java.util.Arrays;
  * This class looks like it's meant to provide a few public static methods
  * for searching and sorting arrays.  It also has a main method that tests
  * the searching and sorting methods.
- * 
+ *
  * TODO: The search and sort methods in this class contain bugs that can
- * cause incorrect output or infinite loops.  Use the Eclipse debugger to 
+ * cause incorrect output or infinite loops.  Use the Eclipse debugger to
  * find the bugs and fix them
- * 
+ *
  * @author Anonymous
- * 
+ *
  */
 public class BuggySearchAndSort {
-	
+
 	public static void main(String[] args) {
-		
+
 		int[] A = new int[10];  // Create an array and fill it with small random ints.
 		for (int i = 0; i < 10; i++)
 			A[i] = 1 + (int)(10 * Math.random());
-		
+
 		int[] B = A.clone();   // Make copies of the array.
 		int[] C = A.clone();
 		int[] D = A.clone();
-		
+
 		System.out.print("The array is:");
 		printArray(A);
-		
+
 		if (contains(A,5))
 			System.out.println("This array DOES contain 5.");
 		else
 			System.out.println("This array DOES NOT contain 5.");
-		
+
 		Arrays.sort(A);  // Sort using Java's built-in sort method!
 		System.out.print("Sorted by Arrays.sort():  ");
 		printArray(A);   // (Prints a correctly sorted array.)
@@ -61,13 +61,13 @@ public class BuggySearchAndSort {
 		selectionSort(C);
 		System.out.print("Sorted by Selection Sort: ");
 		printArray(C);
-		
+
 		insertionSort(D);
 		System.out.print("Sorted by Insertion Sort: ");
 		printArray(D);
 
 	}
-	
+
 	/**
 	 * Tests whether an array of ints contains a given value.
 	 * @param array a non-null array that is to be searched
@@ -81,7 +81,7 @@ public class BuggySearchAndSort {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Sorts an array into non-decreasing order.  This inefficient sorting
 	 * method simply sweeps through the array, exchanging neighboring elements
@@ -99,10 +99,10 @@ public class BuggySearchAndSort {
 			}
 		}
 	}
-	
+
 	/**
 	 * Sorts an array into non-decreasing order.  This method uses a selection
-	 * sort algorithm, in which the largest item is found and placed at the end of 
+	 * sort algorithm, in which the largest item is found and placed at the end of
 	 * the list, then the second-largest in the next to last place, and so on.
 	 */
 	public static void selectionSort(int[] array) {
@@ -117,7 +117,7 @@ public class BuggySearchAndSort {
 			array[positionOfMax] = temp;
 		}
 	}
-	
+
 	/**
 	 * Sorts an array into non-decreasing order.  This method uses a standard
 	 * insertion sort algorithm, in which each element in turn is moved downwards
@@ -135,7 +135,7 @@ public class BuggySearchAndSort {
 			array[pos + 1] = temp;  // place temp into last vacated position
 		}
 	}
-	
+
 	/**
 	 * Outputs the ints in an array on one line, separated by spaces,
 	 * with a line feed at the end.
