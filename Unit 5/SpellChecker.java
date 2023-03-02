61a3a482d2079d26681cdad9fd319f0eaa5d0381
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-03-01 23:39:20 -0500

    Add assignment file

--- Unit 5/SpellChecker.java
+++ Unit 5/SpellChecker.java
@@ -1,4 +1,4 @@
-package Unit_5;
+//package Unit_5;
 
 import java.io.*;
 import java.util.*;
@@ -6,30 +6,30 @@ import javax.swing.JFileChooser;
 
 
 /**
- * 
- * 				
+ *
+ *
  * 				   			 Programming Assignment Unit 4
  * 								University of the People
  *								 Programming 2 CS 1103
  *								 Eman Omar, instructor
  *								   March 1, 2023
- * 										
- * 
+ *
+ *
  */
 
 
 
 /**
  * A program that acts as a spell checker for a text file of English words.
- * 
+ *
  * The program prompts the user for a text file of properly spelled words
  * to use as a dictionary for spell checking purposes.  Next, the user is
  * prompted for a text file to spell check.  The words in both text files
- * should be separated by one or more non-letter characters.  
+ * should be separated by one or more non-letter characters.
  * <p>
  * If a word in the spell checked file is not found in the dictionary, the
  * program provides suggestions for possible correct spellings.
- * 
+ *
  * @author Anonymous
  */
 public class SpellChecker {
@@ -37,11 +37,11 @@ public class SpellChecker {
 	/**
 	 * Prompts the user for file inputs and calls the
 	 * routines for spell checking and suggestion output.
-	 * 
+	 *
 	 * @param args Command line args (not used).
 	 */
 	public static void main(String[] args) {
-		
+
 		/*
 		 * Store the dictionary words in a HashSet for maximum
 		 * efficiency, but declare the reference variable as the
@@ -57,20 +57,20 @@ public class SpellChecker {
 		 * program.
 		 */
 		Collection<String> dictionary = new HashSet<>();
-		
+
 		System.out.println("Choose the file to be used as a dictionary.\n");
 		dictionary = createDictionary();
-		
+
 		if (dictionary.size() == 0) {
 			System.out.println("No dictionary from which to perform spell "
 				+ "checking operations.  Goodbye.");
 			System.exit(0);
 		}
-		
+
 		System.out.println("Choose the file to spell check.\n");
-		
+
 		File file = getInputFileNameFromUser("Select File to spell check");
-		
+
 		if (file != null) {
 			System.out.println("Below is the list of potentially misspelled "
 				+ "words and some possible correct spellings:\n");
@@ -79,72 +79,72 @@ public class SpellChecker {
 			System.out.println("No file selected to spell check.  Goodbye.");
 		}
 	}
-	
+
 	/**
 	 * Obtains a user-selected file to be implemented as a dictionary.
 	 * The words in the dictionary file should be separated by one or more
 	 * non-letter characters.
-	 * 
+	 *
 	 * @return The dictionary that has been filled with words
 	 *     from the user's selected file.
 	 */
 	private static Collection<String> createDictionary()  {
-		
+
 		Collection<String> dict = new HashSet<>();
-		
+
 		File file = getInputFileNameFromUser("Select Dictionary File");
-		
+
 		if (file != null) {
 			try {
 				Scanner filein = new Scanner(file);
-				
+
 				while (filein.hasNext()) {
 					String word = filein.next();
 					word = word.toLowerCase();
 					dict.add(word);
-				}		
+				}
 				filein.close();
-				
+
 			} catch (FileNotFoundException e) {
 				System.out.println("Can't find dictionary file.  No words "
 					+ "added to dictionary.");
 			}
 		}
-		
+
 		return dict;
 	}
-	
+
 	/**
 	 * Use <code>dict</code> to compare against words from the file.
-	 * 
+	 *
 	 * The words in the file to be spell checked should be separated
 	 * by one or more non-letter characters.  If a word is not in the
 	 * dictionary supplied, possible correct suggestions are printed
 	 * to standard output for each word in the input that does not
 	 * match a word in the dictionary.
-	 * 
+	 *
 	 * @param f The file object that has been checked to ensure it
 	 *     is not null.
 	 * @param dict The dictionary to use as the correct spellings
 	 *     of English words.
 	 */
 	private static void spellCheckWordsFromFile(File f, Collection<String> dict) {
-		
-		/* 
+
+		/*
 		 * Store words that have already been output in a set so
 		 * the program will not output the same misspelled word
 		 * more than once.
 		 */
 		Collection<String> wordsToOutput = new HashSet<>();
-		
+
 		try {
 			Scanner in = new Scanner(f);
 			in.useDelimiter("[^a-zA-Z]+");
-			
+
 			while (in.hasNext()) {
 				String word = in.next();
 				word = word.toLowerCase();
-				
+
 				if (!dict.contains(word)) {
 					if (!wordsToOutput.contains(word)) {
 						outputSuggestions(word, dict);
@@ -157,22 +157,22 @@ public class SpellChecker {
 			System.out.println("Can't find file to spell check words.");
 		}
 	}
-	
+
 	/**
 	 * Prints potentially misspelled words along with possible corrections.
-	 * 
+	 *
 	 * @param badWord The word that is potentially misspelled.
 	 * @param dict The dictionary to use as the correct spellings
 	 *     of English words.
 	 */
 	private static void outputSuggestions(String badWord, Collection<String> dict) {
-		
+
 		/*
 		 * Need to declare suggestions as a reference variable of type
 		 * TreeSet in order to use TreeSet-specific methods.
 		 */
 		TreeSet<String> suggestions = new TreeSet<>();
-		
+
 		/*
 		 * Trying to reduce the size and narrow down the responsibilities
 		 * of this functionality by splitting this into a second routine.
@@ -180,18 +180,18 @@ public class SpellChecker {
 		 * suggestions.
 		 */
 		suggestions.addAll(corrections(badWord, dict));
-		
+
 		if (suggestions.size() == 0) {
 			System.out.println(badWord + ": (no suggestions)");
 		} else {
-			
+
 			/*
 			 * Adapted Eck's code snippet from 10.4.2 to properly
 			 * print elements separated with commas from a TreeSet
 			 * of Strings.
 			 */
 			System.out.print(badWord + ": ");
-			String firstWord = suggestions.first();  
+			String firstWord = suggestions.first();
 			System.out.print(firstWord);
 			for (String word : suggestions.tailSet(firstWord, false)) {
 				System.out.print(", " + word);
@@ -199,10 +199,10 @@ public class SpellChecker {
 			System.out.println();
 		}
 	}
-	
+
 	/**
 	 * Collects all the possible corrections to a misspelled word.
-	 * 
+	 *
 	 * @param badWord The word that is potentially misspelled.
 	 * @param dict The dictionary to use as the correct spellings
 	 *     of English words.
@@ -210,18 +210,18 @@ public class SpellChecker {
 	 *     misspelled word.
 	 */
 	private static Collection<String> corrections(String badWord, Collection<String> dict) {
-		
+
 		Collection<String> corrections = new TreeSet<>();
-		
+
 		// Variables to hold the five possible corrections the program considers.
 		String deletedLetters;
 		String changedLetters;
 		String insertedLetters;
 		String swappedLetters;
 		String spaceInserted;
-		
+
 		for (int i = 0; i < badWord.length(); i++) {
-			
+
 			/*
 			 * Test each character of badWord with i and i + 1
 			 * swapped.  Stops before length() - 1 to avoid an
@@ -232,14 +232,14 @@ public class SpellChecker {
 				char temp = c[i];
 				c[i] = c[i + 1];
 				c[i + 1] = temp;
-				
+
 				swappedLetters = new String(c);
-				
+
 				if (dict.contains(swappedLetters)) {
 					corrections.add(swappedLetters);
 				}
 			}
-			
+
 			/*
 			 * Check each position that could have a space inserted, testing if the two
 			 * separate words are in the dictionary.
@@ -248,42 +248,42 @@ public class SpellChecker {
 				spaceInserted = badWord.substring(0, i) + ' ' + badWord.substring(i);
 				corrections.add(spaceInserted);
 			}
-			
+
 			/*
 			 * Check each position with all of the letters a-z for letter deletions,
 			 * changes, and insertions.
 			 */
 			for (char ch = 'a'; ch <= 'z'; ch++) {
-				
+
 				deletedLetters = badWord.substring(0, i) + badWord.substring(i + 1);
 				if (dict.contains(deletedLetters)) {
 					corrections.add(deletedLetters);
 				}
-				
+
 				changedLetters = badWord.substring(0, i) + ch + badWord.substring(i + 1);
 				if (dict.contains(changedLetters)) {
 					corrections.add(changedLetters);
 				}
-				
+
 				insertedLetters = badWord.substring(0, i) + ch + badWord.substring(i);
 				if (dict.contains(insertedLetters)) {
 					corrections.add(insertedLetters);
-					
+
 				// Also need to check adding each letter to end of badWord.
 				} else if (dict.contains(badWord + ch)) {
 					corrections.add(badWord + ch);
 				}
 			}
 		}
-		
+
 		return corrections;
 	}
-	
+
     /**
      * Lets the user select an input file using a standard file
      * selection dialog box.  If the user cancels the dialog
      * without selecting a file, the return value is null.
-     * 
+     *
      * @param title The title of the file dialog.
      * @return The user's file or null if the dialog is cancelled
      *     or closed.
